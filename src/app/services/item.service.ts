42c6a250b0729805813cff3c95a32bb576de6821
BP <chuminhkhoi3110@gmail.com>
2023-02-24 23:28:05 +0700

    push lan 1

---
+++ src/app/services/item.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll()
+  {
+    return this.http.get(`${environment.api_url}item`);
+  }
+}
